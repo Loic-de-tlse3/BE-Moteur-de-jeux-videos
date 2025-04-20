6d5a712fdd85298b122bf428b9c5df7a6f8bd170
Loic-de-tlse3 <145675112+Loic-de-tlse3@users.noreply.github.com>
2025-04-20 02:42:39 +0200

    Ajout génération d'objets

--- brique-de-gameplay/scripts/objet.gd
+++ brique-de-gameplay/scripts/objet.gd
@@ -1,10 +1,24 @@
 extends Node2D
 
+enum Type {
+	DISTANCE,
+	MELE
+}
+
 export (String) var type
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func generation(objets_possibles, emplacement:int):
+	var nouvel_objet = null
+	if randi()%2 == Type.DISTANCE:
+		nouvel_objet = objets_possibles[0].instance()
+	else:
+		nouvel_objet = objets_possibles[1].instance()
+	add_child(nouvel_objet)
+	return nouvel_objet
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
--- brique-de-gameplay/scripts/tableau.gd
+++ brique-de-gameplay/scripts/tableau.gd
@@ -26,8 +26,9 @@ var second_item_touche = Vector2(0, 0)
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	randomize()
-	x_initial_cadre = x_initial - ((taille_cadre - taille_objet)/2)
-	y_initial_cadre = y_initial - ((taille_cadre - taille_objet)/2)
+	#x_initial_cadre = x_initial - ((taille_cadre - taille_objet)/2)
+	#y_initial_cadre = y_initial - ((taille_cadre - taille_objet)/2)
+	print(x_initial_cadre, y_initial_cadre)
 	ajout_cadre_null()
 	for i in nb_item_max:
 		liste_objets.append(null)
@@ -58,11 +59,16 @@ func pixels_vers_case(pixel_x, pixel_y):
 	var y = round((pixel_y - y_initial) / distance_objet)
 	return Vector2(x, y)
 	
+func case_vers_pixels(case:int):
+	var x = distance_objet*(case%nb_item_ligne)
+	var y = distance_objet*(case/nb_item_ligne)
+	print(Vector2(x, y))
+	return Vector2(x, y)
+	
 func toucher_item():
 	if Input.is_action_just_pressed("ui_interaction"):
 		premier_item_touche = get_global_mouse_position()
 		var position_case1 = pixels_vers_case(premier_item_touche.x, premier_item_touche.y)
-		#TODO vérifier si l'élément touché est le générateur ou un objet
 		var position_liste_elt1 = position_case1.x + (6 * position_case1.y)
 		print(position_liste_elt1)
 		if liste_objets[position_liste_elt1] == null:
@@ -70,14 +76,27 @@ func toucher_item():
 			return 0
 		elif liste_objets[position_liste_elt1].type == "générateur":
 			print("réussi")
+			var emplacement = premiere_case_vide()
+			if emplacement == -1:
+				print("Plus de place")
+				return 1
+			var nouvel_objet = liste_objets[position_liste_elt1].generation(objets, emplacement)
+			nouvel_objet.position = case_vers_pixels(emplacement)
+			liste_objets[emplacement] = nouvel_objet
+			print(liste_objets)
 			return 0
-			#liste_objets[position_liste_elt1].generation()
 		elif (liste_objets[position_liste_elt1].type == "distance") || (liste_objets[position_liste_elt1].type == "mêlée"):
 			print("gestion click objet")
 	if Input.is_action_just_pressed("ui_interaction"):
 		print("deuxième if")
 		second_item_touche = get_global_mouse_position()
 		var position_case2 = pixels_vers_case(second_item_touche.x, second_item_touche.y)
+		
+func premiere_case_vide():
+	for i in range(nb_item_max):
+		if liste_objets[i] == null:
+			return i
+	return -1
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
