7aaa860b0558281e97396c438f745cb2d92c34bd
Loic-de-tlse3 <145675112+Loic-de-tlse3@users.noreply.github.com>
2025-04-29 17:03:49 +0200

    Version pour Godot 4.x

---
+++ Version pour Godot 4.x/brique-de-gameplay/scripts/objet.gd
@@ -0,0 +1,43 @@
+extends Node2D
+
+enum Type {
+	DISTANCE,
+	MELE
+}
+
+@export var type: String
+@export var niveau: int
+
+#var tableau_instance = preload("res://Scenes/FenêtreDeJeu.tscn").instance()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func generation(tableau, objets_possibles, emplacement):
+	var nouvel_objet = null
+	if randi()%2 == Type.DISTANCE:
+		nouvel_objet = objets_possibles[0].instantiate()
+	else:
+		nouvel_objet = objets_possibles[1].instantiate()
+	add_child(nouvel_objet)
+	nouvel_objet.position = case_vers_pixels(tableau, emplacement)
+	nouvel_objet.niveau = 1
+	return nouvel_objet
+	
+func fusion(liste_objets, second_objet):
+	if (niveau < 3) and (self.type == second_objet.objet.type) and (self != second_objet.objet) and (self.niveau == second_objet.objet.niveau): 
+		niveau += 1
+		liste_objets[second_objet.position_liste] = null
+		second_objet.objet.queue_free()
+	else:
+		print("Fusion impossible")
+	
+func case_vers_pixels(tableau, case:int):
+	var x = tableau.distance_objet*(case%tableau.nb_item_ligne)
+	var y = tableau.distance_objet*(case/tableau.nb_item_ligne)
+	return Vector2(x, y)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
---
+++ Version pour Godot 4.x/brique-de-gameplay/scripts/tableau.gd
@@ -0,0 +1,118 @@
+extends Node2D
+
+# Variables de tableau
+@export var nb_item_ligne: int
+@export var nb_item_max: int
+@export var x_initial: int
+@export var y_initial: int
+@export var taille_cadre: int
+@export var taille_objet: int
+@export var x_initial_cadre: int
+@export var y_initial_cadre: int
+@export var distance_objet: int
+
+var cadres = [
+	preload("res://Scenes/Cadre_null.tscn"),
+	preload("res://Scenes/Cadre_nv_1.tscn"),
+	preload("res://Scenes/Cadre_nv_2.tscn"),
+	preload("res://Scenes/Cadre_nv_3.tscn")
+]
+
+var generateur = preload("res://Scenes/générateur.tscn")
+var objets = [
+	preload("res://Scenes/arme distance.tscn"),
+	preload("res://Scenes/arme mêlée.tscn")
+]
+
+var liste_objets = []
+
+var objet1 = {
+	"coordonnees": Vector2(0, 0),
+	"position_liste": 0,
+	"position_case": Vector2(0, 0),
+	"objet": null
+}
+
+var objet2 = {
+	"coordonnees": Vector2(0, 0),
+	"position_liste": 0,
+	"position_case": Vector2(0, 0),
+	"objet": null
+}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+	#x_initial_cadre = x_initial - ((taille_cadre - taille_objet)/2)
+	#y_initial_cadre = y_initial - ((taille_cadre - taille_objet)/2)
+	for i in nb_item_max:
+		liste_objets.append(null)
+	ajout_cadres()
+	ajout_generateur()
+
+func ajout_cadres():
+	var x = x_initial_cadre
+	var y = y_initial_cadre
+	var cadre = null
+	for i in liste_objets.size():
+		if liste_objets[i] == null:
+			cadre = cadres[0].instantiate()
+		else:
+			var niveau = liste_objets[i].niveau
+			cadre = cadres[niveau].instantiate()
+		add_child(cadre)
+		cadre.position = Vector2(x, y)
+		x += distance_objet
+		if (i+1) % nb_item_ligne == 0:
+			x = x_initial_cadre
+			y += distance_objet
+			
+func ajout_generateur():
+	var bouton = generateur.instantiate()
+	add_child(bouton)
+	bouton.position = Vector2(x_initial, y_initial)
+	liste_objets[0] = bouton
+	
+	
+func pixels_vers_case(pixel_x, pixel_y):
+	var x = round((pixel_x - x_initial) / distance_objet)
+	var y = round((pixel_y - y_initial) / distance_objet)
+	return Vector2(x, y)
+	
+func toucher_item():
+	if Input.is_action_just_pressed("ui_interaction"):
+		objet1.coordonnees = get_global_mouse_position()
+		objet1.position_case = pixels_vers_case(objet1.coordonnees.x, objet1.coordonnees.y)
+		objet1.position_liste = objet1.position_case.x + (6 * objet1.position_case.y)
+		objet1.objet = liste_objets[objet1.position_liste]
+		if liste_objets[objet1.position_liste] == null:
+			return 0
+		elif liste_objets[objet1.position_liste].type == "générateur":
+			var emplacement = premiere_case_vide()
+			if emplacement == -1:
+				print("Plus de place")
+				return 1
+			var nouvel_objet = liste_objets[objet1.position_liste].generation(self, objets, emplacement)
+			liste_objets[emplacement] = nouvel_objet
+			return 0
+	if Input.is_action_just_released("ui_interaction"):
+		objet2.coordonnees = get_global_mouse_position()
+		objet2.position_case = pixels_vers_case(objet2.coordonnees.x, objet2.coordonnees.y)
+		objet2.position_liste = objet2.position_case.x + (6 * objet2.position_case.y)
+		objet2.objet = liste_objets[objet2.position_liste]
+		if (objet1.objet != null) and (objet2.objet != null):
+			objet2.objet.fusion(liste_objets, objet1)
+		else:
+			print("Fusion impossible")
+		
+func premiere_case_vide():
+	for i in range(nb_item_max):
+		if liste_objets[i] == null:
+			return i
+	return -1
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	toucher_item()
+	ajout_cadres()
+#	pass
